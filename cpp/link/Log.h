5bd8e3aa59946568521b44793d20c6428f070840
Sheng Li <sheng.s.li@outlook.com>
2019-06-07 15:09:41 +0100

    add cpp folder

---
+++ cpp/link/Log.h
@@ -0,0 +1,10 @@
+#pragma once
+
+void Log(const char* message);
+
+
+
+
+
+
+
---
+++ cpp/link/Math.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Log.h"
+
+void Log(const char* message);
+
+int Multiply(int a, int b)
+{
+	Log("Multiply");
+	return a * b;
+}
+
+int main()
+{
+	std::cout << Multiply(5,8) << std::endl;
+}
